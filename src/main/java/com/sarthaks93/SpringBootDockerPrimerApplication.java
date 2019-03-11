35ddc3ec2f4b7a081161fb14f6b43bcb83d67018
sarthak <sarthaks93@gmail.com>
2019-03-12 00:29:01 +0530

    init commit

---
+++ src/main/java/com/sarthaks93/SpringBootDockerPrimerApplication.java
@@ -0,0 +1,13 @@
+package com.sarthaks93;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerPrimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerPrimerApplication.class, args);
+	}
+
+}
